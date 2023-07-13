678b911e5fa735d9990270ea80135c379ff99c44
codekgs <codekgs420@gmail.com>
2023-07-13 21:50:46 +0530

    nn

--- lib/Screen/app.dart
+++ lib/Screen/app.dart
@@ -7,7 +7,34 @@ class AppScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
    backgroundColor: Colors.black,
+     appBar: AppBar(
+      title: Center(child: Text("Spontify App")),
+       backgroundColor: Colors.grey[900],
+     ),
+      
+    body: GridView.count(
 
+      crossAxisCount: 2,
+      children: List.generate(4, (index)  {
+        return Card(
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget> [
+        const ListTile(
+          leading: Icon(Icons.album),
+              title: Text('The Enchanted Nightingale'),
+              subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+              
+              
+        )
+      ],
+    ),
+        );
+           
+        
+      }),
+    ),
     );
   }
 }
+
